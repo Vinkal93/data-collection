db07b0888b3c67e8833fa362fd6655c9a76011d4
Quantum Institute <vinkal93041@gmail.com>
2024-12-04 15:50:02 +0530

    New thankyou page added

---
+++ css/js/script.js
@@ -0,0 +1,6 @@
+// You can add any dynamic functionality here if needed
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Thank you page loaded!');
+    // Add any additional JavaScript functionality here
+});
+
